043ad9decb3138007389a9b54cd9facd4ed35ad7
Vincent <vshan0816@gmail.com>
2021-09-23 16:32:49 -0400

    commit tree 4ddccc975f28f307e53f688eb6007f8445675620

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance < 42) {
+        return 42 - distance
+    } else if (distance > 42){
+        return distance - 42
+    }
+
+}
+function distanceFromHqInFeet(distance){
+    return distanceFromHqInBlocks(distance) * 264
+}
+function distanceTravelledInFeet(num1,num2) {
+    const difference = Math.max(num1,num2) - Math.min(num1,num2)
+    return difference * 264
+}
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far'
+    } else if (distanceTravelledInFeet(start, destination) > 2000) {
+        return 25
+    } else if (distanceTravelledInFeet(start, destination) > 400) {
+        return (distanceTravelledInFeet(start, destination) -400) *.02
+    }
+}
